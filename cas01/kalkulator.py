210f76a33128c4b9048765bf8349f49e6c3a6970
Aleksandar Ristic <storiaca@gmail.com>
2025-01-16 15:47:15 +0100

    Zadatak 10

---
+++ cas01/kalkulator.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+
+print('Zbir je ' + str(a + b))
+print(f'Razlika je {a-b}')
+print(f'Puta je {a*b}')
+
+
---
+++ cas01/zad10.py
@@ -0,0 +1,8 @@
+landscape = float(input())
+price = landscape * 7.61
+discount = price * 0.18
+
+final_price = price - discount
+
+print(f'The final price is: {final_price} USD.')
+print(f'The discount is: {discount} USD.')
