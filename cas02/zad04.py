7cceeefae518e0a3194a4f76d4dfc6570f789aa2
Aleksandar Ristic <storiaca@gmail.com>
2025-01-17 12:57:26 +0100

    Zadatak 04: Vaation Books List

---
+++ cas02/zad04.py
@@ -0,0 +1,12 @@
+# Vacation Books List
+#import math
+number_of_pages = int(input())
+pages = int(input())
+days = int(input())
+
+reading_time = number_of_pages / pages
+required_hours = reading_time // days
+
+# print(int(required_hours))
+
+# print(math.floor((number_of_pages / pages) / days))
