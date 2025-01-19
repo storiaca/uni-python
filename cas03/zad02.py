98f90ad950e28f8e0391e2a8a3919187836ec7be
Aleksandar Ristic <storiaca@gmail.com>
2025-01-19 14:55:18 +0100

    Zadatak 2: Greater Number

---
+++ cas03/zad02.py
@@ -0,0 +1,11 @@
+'''
+2. Greater Number
+Write a program that reads two integers entered by the user and prints the larger of the two.
+'''
+first_number = int(input())
+second_number = int(input())
+
+if first_number > second_number:
+    print(first_number)
+else:
+    print(second_number)
