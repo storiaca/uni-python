e8a687148233d2e5ee37f56540dbb5bbbaf1a63e
Aleksandar Ristic <storiaca@gmail.com>
2025-01-19 14:49:52 +0100

    Zadatak 1: Excellent Result

---
+++ cas03/zad01.py
@@ -0,0 +1,8 @@
+'''
+1. Excellent Result
+Write a console program that reads a rating (integer) entered by the user and prints "Excellent!" if the score is 5 or higher.
+'''
+rating = float(input())
+
+if rating >= 5.5:
+   print("Excellent!")
