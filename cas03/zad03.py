eefaa4c51bf95772cab193817b9afe2c8e184759
Aleksandar Ristic <storiaca@gmail.com>
2025-01-19 14:56:58 +0100

    Zadatak 3: Even or Odd number

---
+++ cas03/zad03.py
@@ -0,0 +1,10 @@
+'''
+3. Even or Odd
+Write a program that reads an integer entered by the user and prints whether it is even or odd. 
+'''
+number = int(input())
+
+if number % 2 == 0:
+    print('even')
+else:
+    print('odd')
