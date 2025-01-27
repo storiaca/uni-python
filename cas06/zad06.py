1bd679485d70bb8ac84662e4d1cf4c81b5b64aed
Aleksandar Ristic <storiaca@gmail.com>
2025-01-27 18:24:13 +0100

    Zadatak 6: Operations Between umbers tekst

---
+++ cas06/zad06.py
@@ -0,0 +1,4 @@
+'''
+6. Operations Between Numbers
+Write a program that reads two integers (N1 and N2) and an operator with which to perform a mathematical operation with them. Possible operations are: Addition (+), Subtraction (-), Multiplication (*), Division (/) and Division with remainder (%). When adding, subtracting, and multiplying the console, the result must be printed and whether it is even or odd. In ordinary division - the result. In modular division - the remainder. It should be borne in mind that the divisor can be equal to 0 (zero) and not divisible by zero. In this case, a specific message must be printed.
+'''
