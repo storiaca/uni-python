3991253e27a285db00efa4a15e83058337c062b8
Aleksandar Ristic <storiaca@gmail.com>
2025-01-27 18:25:51 +0100

    Zadatak 7: Hotel room tekst

---
+++ cas06/zad07.py
@@ -0,0 +1,4 @@
+'''
+7. Hotel Room
+The hotel offers 2 types of rooms: studio and apartment. Write a program that calculates the price for the entire stay for a studio and apartment. Prices depend on the month of stay:
+'''
