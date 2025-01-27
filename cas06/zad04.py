6af2da346d95e27aa330c08d2c086ef222049a4b
Aleksandar Ristic <storiaca@gmail.com>
2025-01-27 18:21:27 +0100

    Zadatak 4: Fishing boat tekst

---
+++ cas06/zad04.py
@@ -0,0 +1,12 @@
+'''
+4. Fishing Boat
+Tony and his friends loved to go fishing. They were so passionate about fishing that they decided to go fishing by boat. The price of renting a boat depends on the season and the number of fishermen.
+The price depends on the season: 
+    • The price for renting a boat in the spring is 3000 USD
+    • The price for renting a boat in summer and autumn is 4200 USD.
+    • The price for renting a boat in winter is 2600 USD.
+Depending on the number of people, the group receives a discount:
+    • If the group is up to 6 people inclusive – a 10% discount.
+    • If the group is from 7 to 11 people inclusive - a 15% discount
+    • If the group is more than 12 people - a 25% discount 
+'''
