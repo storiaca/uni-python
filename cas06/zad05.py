a72fe82c8023378f0bbde2027bbea80a0ca36368
Aleksandar Ristic <storiaca@gmail.com>
2025-01-27 18:22:52 +0100

    Zadatak 5: Journey tekst

---
+++ cas06/zad05.py
@@ -0,0 +1,5 @@
+'''
+5. Journey
+Strangely, most people plan their vacation early. A young programmer has an exact budget and free time for each season. Write a program that will accept the budget and the season, and print, where the programmer will rest and how much he will spend.
+The budget determines the destination, and the season determines how much of the budget he will spend. If it is summer, he will rest at the campsite and in the winter at a hotel. If he is in Europe, he will stay in a hotel, regardless of the season. Each campsite or hotel, according to the destination, has its price that corresponds to a certain percentage of the budget:
+'''
