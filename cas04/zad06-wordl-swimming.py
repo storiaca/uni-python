0ee4fb9573e712314aa6314b7d32e7ad3e71db5e
Aleksandar Ristic <storiaca@gmail.com>
2025-01-31 10:29:24 +0100

    Zadatak 6: World Swimming Record fix

--- cas04/zad06-wordl-swimming.py
+++ cas04/zad06-wordl-swimming.py
@@ -3,6 +3,8 @@
 Oliver decides to break the World Record for long-distance swimming. On the console, we type: the record that Oliver has to break, the distance in meters he has to swim, and the time in seconds for which he swims a distance of 1 m. To write a program that calculates whether he has done the record, it must be considered that: the resistance of the water slows him down every 15 m by 12.5 seconds. After calculating how many seconds Oliver will slow down, the result should be rounded down to the nearest integer number.
 Calculate the time in seconds for which Ivan will swim the distance and the difference from the World Record. 
 '''
+import math
+
 record = float(input())
 distance = float(input())
 time_for_meter = float(input())
@@ -11,7 +13,9 @@ time_for_meter = float(input())
 
 time = distance * time_for_meter
 
-time += (distance // 15) * 12.5
+# time += (distance // 15) * 12.5
+# mora zato sto se koriste float tip brojevi
+time += (math.floor(distance/15)) * 12.5
 
 if(record > time):
     print(f'Yes, he succeeded! The new world record is {time:.2f} seconds.')
