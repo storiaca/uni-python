db5cf6ff344e1c509e96adbe63c5b85ad69c6952
Aleksandar Ristic <storiaca@gmail.com>
2025-02-01 12:18:33 +0100

    Zadatak 5: Character Sequence

---
+++ cas07/zad05-print-char.py
@@ -0,0 +1,8 @@
+'''
+5. Character Sequence
+Write a program that receives text (string) and prints each character of the string on a separate line.
+'''
+characters = input()
+
+for i in range(0, len(characters)):
+    print(characters[i])
