69dd53ac550a21aec9776e1819fc1079db0e1d1a
Aleksandar Ristic <storiaca@gmail.com>
2025-02-22 18:18:27 +0100

    Zadatak 3: Combinations drugi nacin, brzi

--- cas11/zad03_combination.py
+++ cas11/zad03_combination.py
@@ -10,3 +10,14 @@ for i in range(0, num + 1):
             if (i + j + z == num):
                 result +=1
 print(result)
+
+# drugi nacin bez jednog for loop-a
+
+num = int(input())
+result = 0
+for i in range(0, num + 1):
+    for j in range (0, num + 1):
+        z = num - (i + j)
+        if (0 <= z <= num):
+            result +=1
+print(result)
